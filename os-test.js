e7bcf1195091e9ad1f03a42c370330d2cfe1e527
horusjavier <kornjaviersanchezb@gmail.com>
2022-09-24 23:54:52 -0500

    funciones de OS de node

--- os-test.js
+++ os-test.js
@@ -1,5 +1,23 @@
 const OS = require('OS');
 
 console.log(OS.type());
+console.log(OS.homedir());
+console.log(OS.cpus());
+console.log(OS.devNull);
+console.log(OS.endianness());
+console.log(OS.freemem());
+console.log(OS.hostname());
+console.log(OS.loadavg());
+console.log(OS.networkInterfaces());
+console.log(OS.platform());
+console.log(OS.release());
+console.log(OS.tmpdir());
+console.log(OS.uptime());
+console.log(OS.version());
+
+//No funciona en versiones menores a 18.9.0
+//console.log(OS.machine());
+
+
 
 console.log(process.memoryUsage());
