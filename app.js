9d7c5eb4405db760fe168b6154c941dcecb27eb4
horusjavier <kornjaviersanchezb@gmail.com>
2022-09-24 22:40:25 -0500

    Usando Desestructuración para solo llamar una funcion o mas separados por ,  en especifico tambien se puede usar como una variable y llamarla directamente

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const  saludo  = require("./saludar");
-const  { Uno } = require("./desestructuracion.js")
+const  { Uno, Dos } = require("./desestructuracion.js")
 
 console.log(saludo.saludar("Hola mundo"));
 console.log(saludo.saludarHolamudo());
 console.log(saludo["saludo mundo"]());
 
 console.log(Uno());
+console.log(Dos());
