58daeedae51254cacb2a6cdf731b6a70ec83b3c3
horusjavier <kornjaviersanchezb@gmail.com>
2022-09-26 22:52:33 -0500

    funciones setImmediate ejemplos

--- timer.js
+++ timer.js
@@ -2,6 +2,10 @@ function saludaTiempo(mensaje){
 console.log(`Hola en tiempo programado ${mensaje}`)
 }
 
+function enviarMesnaje(mensaje){
+  console.log(`este el mesnaje ${mensaje}`)
+  }
+
 //saludaTiempo('10000 segundos');
 
 function sumar(a, b){
@@ -10,6 +14,11 @@ function sumar(a, b){
 
 //Higer-Order Programming, llamar una dentro de otra funcion
 setTimeout(saludaTiempo, 10000, '10000 segundos');
-
-
 setTimeout(sumar, 20000, 10, 10);
+
+//Se Ejecuta primero
+console.log('Antes de setImmediate');
+//Se ejecuta tercero
+setImmediate(enviarMesnaje,'ejecutando setImmediate()');
+//Se ejecuta Segundo
+console.log('Despues de setImmediate');
