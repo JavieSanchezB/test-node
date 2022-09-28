4c1d3382b8374e1bf8440204fb06b11ce69591c6
horusjavier <kornjaviersanchezb@gmail.com>
2022-09-27 21:50:50 -0500

    funcion rename

--- module-fs.js
+++ module-fs.js
@@ -2,10 +2,21 @@ const fs = require('fs');
 
 fs.readFile('index.html', 'utf-8', (err, contenido) => {
 if(err){
-    console.log(err);
+    //console.log(err);
+   
+   //muestra la liena donde se encueta el error y detiene la ejecucion
+    throw err;
+
 } else {
     console.log(contenido);
 }
 
+});
 
+fs.rename('archivo.html','main.js', (err) => {
+if(err){
+    throw err;
+}else{
+    console.log('Se cambio el nombre del archivo correctamente')
+}
 });
