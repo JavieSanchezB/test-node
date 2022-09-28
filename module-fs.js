9e3400a3b9147379a896a63df9de6a7ca3b45be5
horusjavier <kornjaviersanchezb@gmail.com>
2022-09-27 22:02:09 -0500

    funcion appenFile agregar informacion al final del archivo

--- module-fs.js
+++ module-fs.js
@@ -1,22 +1,30 @@
 const fs = require('fs');
 
 fs.readFile('index.html', 'utf-8', (err, contenido) => {
-if(err){
+  if (err) {
     //console.log(err);
-   
-   //muestra la liena donde se encueta el error y detiene la ejecucion
+
+    //muestra la liena donde se encueta el error y detiene la ejecucion
     throw err;
 
-} else {
+  } else {
     console.log(contenido);
-}
+  }
 
 });
 
-fs.rename('archivo.html','main.js', (err) => {
-if(err){
+// fs.rename('archivo.html','main.js', (err) => {
+// if(err){
+//     throw err;
+// }else{
+//     console.log('Se cambio el nombre del archivo correctamente')
+// }
+// });
+
+fs.appendFile('main.txt', 'este un ejemplo para agrear datos a un archivo al final', (err) => {
+  if (err) {
     throw err;
-}else{
-    console.log('Se cambio el nombre del archivo correctamente')
-}
+  } else {
+    console.log('agraga la informacion al archivo')
+  }
 });
--- saludar.js
+++ saludar.js
@@ -1,21 +1,21 @@
 function saludar($nombre) {
-    return $nombre;
-    }
-function saludarHolamudo(){
-    return 'Hola, mundo!';
+  return $nombre;
 }
-function saludomundo(){
-    return 'Hola, mundo! la funcion tiene espacio';
+function saludarHolamudo() {
+  return 'Hola, mundo!';
+}
+function saludomundo() {
+  return 'Hola, mundo! la funcion tiene espacio';
 
 }
-  //Exportar modulos uno por uno
-  // module.exports.saludar = saludar; 
-  // module.exports.saludarHolamudo = saludarHolamudo; 
+//Exportar modulos uno por uno
+// module.exports.saludar = saludar; 
+// module.exports.saludarHolamudo = saludarHolamudo; 
 
-  //Exportar modulos en un objeto
-  module.exports = {
-    saludar: saludar,
-    saludarHolamudo: saludarHolamudo,
-    'saludo mundo': saludomundo                                                                                                 
+//Exportar modulos en un objeto
+module.exports = {
+  saludar: saludar,
+  saludarHolamudo: saludarHolamudo,
+  'saludo mundo': saludomundo
 
-  };
+};
